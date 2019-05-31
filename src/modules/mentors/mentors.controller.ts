e91a2336b8ddb99143a51609659bfac30b12e48e
Crysfel Villa <crysfel@bleext.com>
2019-05-30 23:58:36 -0500

    Filtering mentors by tags, country, name and language

--- src/modules/mentors/mentors.controller.ts
+++ src/modules/mentors/mentors.controller.ts
@@ -1,16 +1,24 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { ApiBearerAuth, ApiUseTags } from '@nestjs/swagger';
+import { Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiUseTags } from '@nestjs/swagger';
+import { MentorsService } from './mentors.service';
+import { MentorFiltersDto } from './dto/mentorfilters.dto';
+import { User } from '../users/interfaces/user.interface';
 
 @ApiUseTags('/mentors')
 @ApiBearerAuth()
 @Controller('mentors')
 export class MentorsController {
 
+  constructor(private readonly mentorsService: MentorsService) { }
+
+  @ApiOperation({ title: 'Return all mentors in the platform by the given filters' })
   @Get()
-  async index() {
+  async index(@Query() filters: MentorFiltersDto) {
+    const mentors: User[] = await this.mentorsService.findAll(filters);
+
     return {
       success: true,
-      mentors: [],
+      data: mentors,
     };
   }
 
@@ -22,8 +30,10 @@ export class MentorsController {
     };
   }
 
+  @ApiOperation({ title: 'Creates a new request to become a mentor, pending for Admin to approve' })
   @Post('requests')
   async request() {
+    
     return {
       success: true,
     }
--- src/modules/mentors/mentors.service.ts
+++ src/modules/mentors/mentors.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Query, Model } from 'mongoose';
+import { MentorFiltersDto } from './dto/mentorfilters.dto';
 import { UserDto } from '../users/dto/user.dto';
 import { User } from '../users/interfaces/user.interface';
 
@@ -7,11 +8,27 @@ import { User } from '../users/interfaces/user.interface';
 export class MentorsService {
   constructor(@Inject('USER_MODEL') private readonly userModel: Model<User>) { }
 
-  async findAll(): Promise<User[]> {
-    const filters = {
-      roles: ['Mentor'],
+  async findAll(filters: MentorFiltersDto): Promise<User[]> {
+    const onlyMentors:any = {
+      roles: 'Mentor',
     };
 
-    return await this.userModel.find(filters).exec();
+    if (filters.name) {
+      onlyMentors.name = { '$regex': filters.name, '$options': 'i' };
+    }
+
+    if (filters.tags) {
+      onlyMentors.tags = { $all: filters.tags.split(',') };
+    }
+    
+    if (filters.country) {
+      onlyMentors.country = filters.country;
+    }
+    
+    if (filters.spokenLanguages) {
+      onlyMentors.spokenLanguages = filters.spokenLanguages;
+    }
+
+    return await this.userModel.find(onlyMentors).exec();
   }
 }
