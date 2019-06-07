6978a862fd088d33f5b6cd5398ff03308b82b4f0
Brent M Clark <brentmclark@gmail.com>
2019-06-07 00:14:32 -0500

    add CORS support

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({origin: process.env.CORS_ORIGIN || 'https://codingcoach.io'});
   const options = new DocumentBuilder()
     .setTitle('Coding Coach')
     .setDescription('A REST API to serve the content for the alpha site')
@@ -17,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
