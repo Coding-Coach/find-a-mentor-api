bd70382683ed2b6d2dcad811ea0435f0cfb4c572
Brent M Clark <brentmclark@gmail.com>
2019-08-06 12:07:49 -0500

    switch to enum

--- src/modules/email/interfaces/email.interface.ts
+++ src/modules/email/interfaces/email.interface.ts
@@ -1,4 +1,12 @@
+export enum Template {
+  WELCOME_MESSAGE = 'd-1434be390e1b4288b8011507f1c8d786',
+  MENTOR_APPLICATION_RECEIVED = 'd-bf78306901e747a7b3f92761b9884f2e',
+  MENTOR_APPLICATION_APPROVED = 'd-88dc20e5dd164510a32f659f9347824e',
+  MENTOR_APPLICATION_REJECTED = 'd-ad08366d02654587916a41bb3270afed',
+}
+
+
 export interface SendData {
   to: String,
-  templateId: String,
+  templateId: Template,
 }
