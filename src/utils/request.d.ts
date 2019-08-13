9b1904c4d71311417eac0bc58688e056f5f6e08e
Crysfel Villa <crysfel@bleext.com>
2019-08-13 10:33:36 -0500

    Extending the request object to include the user

--- src/utils/request.d.ts
+++ src/utils/request.d.ts
@@ -1,7 +1,7 @@
 import { User } from '../modules/common/interfaces/user.interface';
 
-declare namespace Express {
-  export interface Request {
-    user?: User
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: User,
   }
 }
