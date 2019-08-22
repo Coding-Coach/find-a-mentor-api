dadede988e220dc2dc8f1eb3065e5122843475a9
Crysfel Villa <crysfel@bleext.com>
2019-08-21 23:08:57 -0500

    Returning filters

--- src/modules/common/mentors.service.ts
+++ src/modules/common/mentors.service.ts
@@ -16,7 +16,7 @@ export class MentorsService {
    * Search for mentors by the given filters
    * @param filters filters to apply
    */
-  async findAll(filters: MentorFiltersDto, isLoggedIn: boolean): Promise<User[]> {
+  async findAll(filters: MentorFiltersDto, isLoggedIn: boolean) {
     const onlyMentors: any = {
       roles: 'Mentor',
     };
@@ -38,12 +38,27 @@ export class MentorsService {
       onlyMentors.spokenLanguages = filters.spokenLanguages;
     }
 
-    return await this.userModel.find(onlyMentors)
+    const countries = await this.userModel.findUniqueCountries(onlyMentors);
+    const languages = await this.userModel.find(onlyMentors)
+      .distinct('spokenLanguages');
+    const technologies = await this.userModel.find(onlyMentors)
+      .distinct('tags');
+
+    const mentors = await this.userModel.find(onlyMentors)
       .select(projections)
       .skip(filters.offset)
       .limit(filters.perpage)
       .sort({ created_at: 'desc' })
       .exec();
+
+    return {
+      mentors,
+      filters: {
+        countries,
+        languages: languages.sort(),
+        technologies: technologies.sort(),
+      },
+    };
   }
 
   async findApplications(filters): Promise<Application[]> {
--- src/modules/common/schemas/user.schema.ts
+++ src/modules/common/schemas/user.schema.ts
@@ -1,5 +1,7 @@
 import * as mongoose from 'mongoose';
+import * as countriesDb from 'i18n-iso-countries';
 import { ChannelName } from '../interfaces/user.interface';
+import { Filter } from '../dto/filter.dto';
 
 export const ChannelSchema = new mongoose.Schema({
   type: {
@@ -40,3 +42,20 @@ export const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.set('timestamps', true);
+
+UserSchema.statics.findUniqueCountries = async function (filters): Promise<Array<Filter>> {
+  const result: Array<Filter> = [];
+
+  const countries = await this.find(filters)
+    .distinct('country');
+
+  countries.sort().forEach((id) => {
+    const label = countriesDb.getName(id, 'en');
+
+    if (label) {
+      result.push(new Filter({ id, label }));
+    }
+  });
+
+  return result;
+};
