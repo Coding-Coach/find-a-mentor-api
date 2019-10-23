49b6445bd78a8624cc82bcf277d8dd91424dcb92
Muthomi Mate <muthomimate@gmail.com>
2019-10-23 20:57:58 +0300

    create get all user mentor lists endpoint

--- src/modules/lists/lists.service.ts
+++ src/modules/lists/lists.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { List } from './interfaces/list.interface';
 import { ListDto } from './dto/list.dto';
+import { isObjectId } from '../../utils/objectid';
 
 @Injectable()
 export class ListsService {
@@ -12,5 +13,11 @@ export class ListsService {
   async createList(data: ListDto): Promise<List> {
     const list = new this.listModel(data);
     return await list.save();
+  }
+
+  async findByUserId(_id: string): Promise<List[]> {
+    if (isObjectId(_id)) {
+      return await this.listModel.find({user: {_id}}).exec();
+    }
   }
 }
