d7ceb4ace2e908105183a8ddea829ef37fe861da
David <davidkwan95@gmail.com>
2019-11-18 23:21:20 +0700

    add delete a mentor list endpoint (#120)
    * add delete a mentor list endpoint
    
    * add addition check if user exist

--- src/modules/lists/lists.controller.ts
+++ src/modules/lists/lists.controller.ts
@@ -1,4 +1,17 @@
-import { BadRequestException, Body, Controller, Get, Post, Param, Req, UnauthorizedException, UsePipes, ValidationPipe, Put } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Req,
+  UnauthorizedException,
+  UsePipes,
+  ValidationPipe,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiUseTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { Role, User } from '../common/interfaces/user.interface';
@@ -11,17 +24,22 @@ import { ListDto } from './dto/list.dto';
 @ApiBearerAuth()
 @Controller('users/:userid/lists')
 export class ListsController {
-
   constructor(
     private readonly usersService: UsersService,
     private readonly listsService: ListsService,
-  ) { }
+  ) {}
 
-  @ApiOperation({ title: 'Creates a new mentor\'s list for the given user' })
+  @ApiOperation({ title: `Creates a new mentor's list for the given user` })
   @Post()
   @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
-  async store(@Req() request: Request, @Param('userid') userId: string, @Body() data: ListDto) {
-    const current: User = await this.usersService.findByAuth0Id(request.user.auth0Id);
+  async store(
+    @Req() request: Request,
+    @Param('userid') userId: string,
+    @Body() data: ListDto,
+  ) {
+    const current: User = await this.usersService.findByAuth0Id(
+      request.user.auth0Id,
+    );
     const user: User = await this.usersService.findById(userId);
 
     // Make sure user exist
@@ -31,7 +49,9 @@ export class ListsController {
 
     // Only admins can create lists for other users
     if (!current._id.equals(user._id) && !current.roles.includes(Role.ADMIN)) {
-      throw new UnauthorizedException('Not authorized to perform this operation');
+      throw new UnauthorizedException(
+        'Not authorized to perform this operation',
+      );
     }
 
     const list: List = await this.listsService.createList({
@@ -45,10 +65,12 @@ export class ListsController {
     };
   }
 
-  @ApiOperation({ title: 'Gets mentor\'s list for the given user' })
+  @ApiOperation({ title: `Gets mentor's list for the given user` })
   @Get()
   async myList(@Req() request, @Param('userid') userId: string) {
-    const current: User = await this.usersService.findByAuth0Id(request.user.auth0Id);
+    const current: User = await this.usersService.findByAuth0Id(
+      request.user.auth0Id,
+    );
     const user: User = await this.usersService.findById(userId);
 
     // check if user exists
@@ -59,9 +81,9 @@ export class ListsController {
 
     // Only current user and admins can view both private and public lists for a user
     if (current._id.equals(user._id) || current.roles.includes(Role.ADMIN)) {
-      lists = await this.listsService.findByUserId({userId});
+      lists = await this.listsService.findByUserId({ userId });
     } else {
-      lists = await this.listsService.findByUserId({userId, public: true });
+      lists = await this.listsService.findByUserId({ userId, public: true });
     }
 
     return {
@@ -73,8 +95,15 @@ export class ListsController {
   @ApiOperation({ title: 'Updates a given list' })
   @Put('/:listid')
   @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
-  async updateList(@Req() request: Request, @Param('userid') userId: string, @Param('listid') listId: string, @Body() data: ListDto) {
-    const current: User = await this.usersService.findByAuth0Id(request.user.auth0Id);
+  async updateList(
+    @Req() request: Request,
+    @Param('userid') userId: string,
+    @Param('listid') listId: string,
+    @Body() data: ListDto,
+  ) {
+    const current: User = await this.usersService.findByAuth0Id(
+      request.user.auth0Id,
+    );
     const user: User = await this.usersService.findById(userId);
 
     // check if user exists
@@ -84,10 +113,16 @@ export class ListsController {
 
     // only admins and current user can update lists
     if (!current._id.equals(user._id) && !current.roles.includes(Role.ADMIN)) {
-      throw new UnauthorizedException('Not authorized to perform this operation');
+      throw new UnauthorizedException(
+        'Not authorized to perform this operation',
+      );
     }
 
-    const list: List[] = await this.listsService.findByUserId({userId, listId, isFavorite: false});
+    const list: List[] = await this.listsService.findByUserId({
+      userId,
+      listId,
+      isFavorite: false,
+    });
 
     // check if list exists
     if (list.length < 1) {
@@ -105,5 +140,40 @@ export class ListsController {
     return {
       success: true,
     };
+  }
+
+  @ApiOperation({ title: `Deletes the given mentor's list for the given user` })
+  @Delete(':listId')
+  async deleteList(
+    @Req() request: Request,
+    @Param('userid') userId: string,
+    @Param('listId') listId: string,
+  ) {
+    const current: User = await this.usersService.findByAuth0Id(
+      request.user.auth0Id,
+    );
+    const user: User = await this.usersService.findById(userId);
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    if (!current._id.equals(user._id) && !current.roles.includes(Role.ADMIN)) {
+      throw new UnauthorizedException(
+        'Not authorized to perform this operation',
+      );
+    }
+
+    try {
+      const res: any = await this.listsService.delete(listId);
+      return {
+        success: res.ok === 1,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error: error.message,
+      };
+    }
   }
 }
