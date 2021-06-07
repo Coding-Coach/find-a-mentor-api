3e540606f05d64fdd7400d2115941494dd451be2
Mosh Feu <moshfeu@gmail.com>
2021-06-08 01:46:13 +0300

    emails touches + run local

--- emails/build.js
+++ emails/build.js
@@ -6,7 +6,6 @@ if (!fs.existsSync('emails/full')) {
 }
 
 fs.readdirSync('emails/templates', { encoding: 'utf-8' }).forEach(file => {
-  console.log(1111, file);
   const part = fs.readFileSync(`emails/templates/${file}`, {
     encoding: 'utf8',
   });
--- emails/show.js
+++ emails/show.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const app = express()
 const port = 3003;
-const layout = fs.readFileSync('content/email_templates/layout.html', {encoding: 'utf8'});
+const layout = fs.readFileSync('emails/layout.html', {encoding: 'utf8'});
 
 function injectData(template, data) {
   return template.replace(/{{(.*?)}}/gm, (_,prop) => data[prop] || `{{${prop}}}`);
@@ -13,7 +13,7 @@ app.get('/:templateName', function (req, res) {
   const {templateName} = req.params;
   if (templateName.includes('.')) return;
   const {data} = req.query;
-  const template = fs.readFileSync(`content/email_templates/${templateName}.html`, {encoding: 'utf8'});
+  const template = fs.readFileSync(`emails/templates/${templateName}.html`, {encoding: 'utf8'});
   const content = injectData(layout.replace('$$$Content$$$', template), JSON.parse(data || '{}'));
   res.send(content);
 });
